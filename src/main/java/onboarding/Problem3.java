98e0acfb1eaff8e573309a0fc4da75a22c33abb5
Be-student <presentsong@naver.com>
2022-10-27 00:27:55 +0900

    feat(Problem3): getClapCount 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,5 +1,8 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Problem3 {
     public static int solution(int number) {
         int answer = 0;
@@ -7,5 +10,30 @@ public class Problem3 {
             answer += getClapCount(i);
         }
         return answer;
+    }
+
+    private static int getClapCount(int currentNumber) {
+        int clap = 0;
+        int parsingNumber = currentNumber;
+        while (parsingNumber > 0) {
+            if (isClapable(parsingNumber)) {
+                clap++;
+            }
+            parsingNumber /= 10;
+        }
+        return clap;
+    }
+
+    private static boolean isClapable(int parsedNumber) {
+        if (parsedNumber % 10 == 3) {
+            return true;
+        }
+        if (parsedNumber % 10 == 6) {
+            return true;
+        }
+        if (parsedNumber % 10 == 9) {
+            return true;
+        }
+        return false;
     }
 }
