68e10c5e82cf46399c4bc7ef3753759b423863c6
Be-student <presentsong@naver.com>
2022-10-26 19:48:28 +0900

    feat(Problem1): 페이지가 범위 안에 없다면 예외 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -4,7 +4,34 @@ import java.util.List;
 
 class Problem1 {
     public static int solution(List<Integer> pobi, List<Integer> crong) {
+        if (!isValidInput(pobi) || !isValidInput(crong)) {
+            return -1;
+        }
         int answer = Integer.MAX_VALUE;
         return answer;
+    }
+
+    private static boolean isValidInput(List<Integer> pages) {
+        if (!isPageListInRange(pages)) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean isPageListInRange(List<Integer> pages) {
+        if (isPageInRange(pages.get(0))) {
+            return false;
+        }
+        if (isPageInRange(pages.get(1))) {
+            return false;
+        }
+        return true;
+    }
+
+    private static boolean isPageInRange(int page) {
+        if (page <= 1 + 1 || page >= 400 - 1) {
+            return false;
+        }
+        return true;
     }
 }
