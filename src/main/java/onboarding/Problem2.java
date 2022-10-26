01d60c161ea98b15cddc668fbb44f0c1f9e2fa1d
Be-student <presentsong@naver.com>
2022-10-26 23:49:53 +0900

    feat(Problem2): 복호화 담당 InnerClass 기능 추가

--- src/main/java/onboarding/Problem2.java
+++ src/main/java/onboarding/Problem2.java
@@ -5,9 +5,41 @@ public class Problem2 {
         return decrypt(cryptogram);
     }
 
-    public static String decrypt(String cryptogram) {
-        StringBuilder decryption = new StringBuilder(cryptogram);
+    private static class Decryption {
+        private StringBuilder decryptBuilder;
+        private int watching;
 
-        return decryption.toString();
+        public Decryption(String decryption) {
+            watching = 0;
+            this.decryptBuilder = new StringBuilder(decryption);
+        }
+
+        public boolean isRemained() {
+            if (watching < decryptBuilder.length() - 1) {
+                return true;
+            }
+            return false;
+        }
+
+        public void searchNext() {
+            watching++;
+        }
+
+        public boolean isDeletable() {
+            if (decryptBuilder.charAt(watching) == decryptBuilder.charAt(watching + 1)) {
+                return true;
+            }
+            return false;
+        }
+
+        public void delete() {
+            decryptBuilder.delete(watching, watching + 2);
+            watching--;
+        }
+
+        @Override
+        public String toString() {
+            return decryptBuilder.toString();
+        }
     }
 }
