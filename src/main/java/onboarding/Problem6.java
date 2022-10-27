ba8e02fa6924d58d32994138004290e778127953
Be-student <presentsong@naver.com>
2022-10-27 09:55:47 +0900

    feat(Problem6): nickname으로 토큰 변환 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -6,5 +6,11 @@ public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         List<String> answer = List.of("answer");
         return answer;
+    private static List<String> getTokens(String nickname) {
+        List<String> tokens = new ArrayList<>();
+        for (int i = 0; i < nickname.length() - 1; i++) {
+            tokens.add(nickname.substring(i, i + 2));
+        }
+        return tokens;
     }
 }
