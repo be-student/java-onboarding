828eff15defbe2eaae3b02246fa9032bbab77a33
Be-student <presentsong@naver.com>
2022-10-27 09:56:43 +0900

    feat(Problem6): 모든 user에 대한 순회 기능 추가

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,14 +1,25 @@
 package onboarding;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Problem6 {
     private static Map<String, String> InvertedMap = new HashMap<String, String>();
     private static Set<String> answer = new HashSet<String>();
 
     public static List<String> solution(List<List<String>> forms) {
-        List<String> answer = List.of("answer");
-        return answer;
+        forms.stream().forEach((user) -> {
+            List<String> tokenWords = getTokens(user.get(1));
+            findDuplicated(user.get(0), tokenWords);
+        });
+
+        return new ArrayList<>(answer);
+    }
+
     private static List<String> getTokens(String nickname) {
         List<String> tokens = new ArrayList<>();
         for (int i = 0; i < nickname.length() - 1; i++) {
