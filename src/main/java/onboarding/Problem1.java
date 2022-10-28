852edb97e27104095b4b90fcc28f71700cdeb397
be-student <presentsong@naver.com>
2022-10-28 16:25:11 +0900

    feat(Problem1): 입력 검증 기능 추가

--- src/main/java/onboarding/Problem1.java
+++ src/main/java/onboarding/Problem1.java
@@ -5,9 +5,14 @@ import onboarding.problem1.Pages;
 import java.util.List;
 
 class Problem1 {
+    private static final int EXCEPTION = -1;
+
     public static int solution(List<Integer> pobi, List<Integer> crong) {
-        Pages pobiChoice=new Pages(pobi);
+        Pages pobiChoice = new Pages(pobi);
         Pages crongCHoice = new Pages(crong);
+        if (pobiChoice.isIllegal() || crongCHoice.isIllegal()) {
+            return EXCEPTION;
+        }
 
         int answer = Integer.MAX_VALUE;
         return answer;
