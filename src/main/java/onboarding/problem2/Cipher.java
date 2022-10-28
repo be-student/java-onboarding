c816f4d9149f61c7166e90b5d2d794f1d8b7820e
be-student <presentsong@naver.com>
2022-10-28 18:11:44 +0900

    feat(Problem2): 암호문 검증 기능 추가

---
+++ src/main/java/onboarding/problem2/Cipher.java
@@ -0,0 +1,34 @@
+package onboarding.problem2;
+
+public class Cipher {
+    private final String cryptogram;
+
+    public Cipher(String cryptogram) {
+        this.cryptogram = cryptogram;
+        isIllegal();
+    }
+
+    private void isIllegal() {
+        isNotNull();
+        isInRange();
+        isOnlySmallCase();
+    }
+
+    private void isNotNull() {
+        if (this.cryptogram == null) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void isInRange() {
+        if (this.cryptogram.length() == 0 || this.cryptogram.length() >= 1001) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private void isOnlySmallCase() {
+        if (!this.cryptogram.toLowerCase().equals(this.cryptogram)) {
+            throw new IllegalArgumentException();
+        }
+    }
+}
