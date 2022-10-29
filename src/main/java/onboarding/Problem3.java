585ff5195b661e16c566f780a17442a92776540f
Be-student <presentsong@naver.com>
2022-10-29 23:16:57 +0900

    feat(Problem3): 범위를 돌며 손벽 횟수 계산 기능 추가

--- src/main/java/onboarding/Problem3.java
+++ src/main/java/onboarding/Problem3.java
@@ -1,8 +1,13 @@
 package onboarding;
 
+import onboarding.problem3.ClapCounter;
+import onboarding.problem3.GameNumber;
+
 public class Problem3 {
     public static int solution(int number) {
-        int answer = 0;
-        return answer;
+        return new GameNumber(number).getRange().reduce(0, (total, currentNumber) -> {
+            total += new ClapCounter(currentNumber).getClapCount();
+            return total;
+        });
     }
 }
--- src/main/java/onboarding/problem3/GameNumber.java
+++ src/main/java/onboarding/problem3/GameNumber.java
@@ -1,5 +1,7 @@
 package onboarding.problem3;
 
+import java.util.stream.IntStream;
+
 public class GameNumber {
     private final int maxRange;
 
@@ -23,5 +25,9 @@ public class GameNumber {
         if (maxRange > 10000) {
             throw new IllegalArgumentException();
         }
+    }
+
+    public IntStream getRange() {
+        return IntStream.rangeClosed(1, maxRange);
     }
 }
