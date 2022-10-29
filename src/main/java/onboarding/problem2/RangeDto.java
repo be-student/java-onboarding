4dc45fb0a39aa11d161cec26e1a9a704912876ad
be-student <presentsong@naver.com>
2022-10-29 22:13:44 +0900

    feat(Problem2): 범위를 담는 dto추가

---
+++ src/main/java/onboarding/problem2/RangeDto.java
@@ -0,0 +1,20 @@
+package onboarding.problem2;
+
+public class RangeDto {
+    private int from;
+    private int to;
+
+    public RangeDto(int from, int to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    public int getFrom() {
+        return from;
+    }
+
+    public int getTo() {
+        return to;
+    }
+}
+
