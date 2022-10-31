249ad17c10eb91e8f12a435715daef564e503430
Be-student <presentsong@naver.com>
2022-10-31 13:30:55 +0900

    fix(Problem6): 정렬해서 return하도록 수정

--- src/main/java/onboarding/Problem6.java
+++ src/main/java/onboarding/Problem6.java
@@ -1,5 +1,7 @@
 package onboarding;
 
+import onboarding.problem6.EmailListDto;
+import onboarding.problem6.EmailSorting;
 import onboarding.problem6.SimilarityChecker;
 import onboarding.problem6.Users;
 
@@ -8,6 +10,7 @@ import java.util.List;
 public class Problem6 {
     public static List<String> solution(List<List<String>> forms) {
         Users validUsers = new Users(forms);
-        return new SimilarityChecker().getSimilarEmails(validUsers).toList();
+        EmailListDto emails = new SimilarityChecker().getSimilarEmails(validUsers);
+        return new EmailSorting(emails).toList();
     }
 }
